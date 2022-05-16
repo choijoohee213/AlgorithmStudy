fcf01d8ea93f96e971d8bc34ba627d551ac3462f
choijoohee213 <choijoohee@naver.com>
2022-05-17 01:32:28 +0900

    220516_프로그래머스_방문길이,불량사용자_최주희

--- Programmers/Level3/220516_프로그래머스_불량 사용자_최주희.java
+++ Programmers/Level3/220516_프로그래머스_불량 사용자_최주희.java
@@ -1,29 +1,61 @@
+// import java.util.*;
+//
+// class Solution {
+// 	Set<Set<String>> result = new HashSet<>();
+// 	public int solution(String[] user_id, String[] banned_id) {
+// 		permutation(0, new HashSet<>(), user_id, banned_id, new boolean[user_id.length]);
+// 		return result.size();
+// 	}
+//
+// 	private void permutation(int cnt, HashSet<String> selected, String[] user_id, String[] banned_id, boolean[] visited) {
+// 		if(cnt == banned_id.length) {
+// 			if(!result.contains(selected)) {
+// 				result.add(new HashSet<>(selected));
+// 			}
+// 			return;
+// 		}
+//
+// 		for (int i = 0; i < user_id.length; i++) {
+// 			if(visited[i] || !isSame(user_id[i], banned_id[cnt])) continue;
+// 			visited[i] = true;
+// 			selected.add(user_id[i]);
+// 			permutation(cnt+1, selected, user_id, banned_id, visited);
+// 			visited[i] = false;
+// 			selected.remove(user_id[i]);
+// 		}
+// 	}
+//
+// 	private boolean isSame(String userId, String bannedId) {
+// 		if(userId.length() != bannedId.length()) return false;
+// 		for (int idx = 0; idx < bannedId.length(); idx++) {
+// 			if(bannedId.charAt(idx) == '*') continue;
+// 			if (bannedId.charAt(idx) != userId.charAt(idx)) return false;
+// 		}
+// 		return true;
+// 	}
+// }
+
+
 import java.util.*;
 
 class Solution {
-	int ans = 0;
-	Set<Set<String>> result = new HashSet<>();
+	Set<Integer> result = new HashSet<>();
 	public int solution(String[] user_id, String[] banned_id) {
-		permutation(0, new HashSet<>(), user_id, banned_id, new boolean[user_id.length]);
-		return ans;
+		permutation(0, 0, user_id, banned_id, new boolean[user_id.length]);
+		return result.size();
 	}
 
-	private void permutation(int cnt, HashSet<String> selected, String[] user_id, String[] banned_id, boolean[] visited) {
+	private void permutation(int cnt, int bits, String[] user_id, String[] banned_id, boolean[] visited) {
 		if(cnt == banned_id.length) {
-			if(!result.contains(selected)) {
-				ans++;
-				result.add(new HashSet<>(selected));
-			}
+			result.add(bits);
 			return;
 		}
 
 		for (int i = 0; i < user_id.length; i++) {
 			if(visited[i] || !isSame(user_id[i], banned_id[cnt])) continue;
 			visited[i] = true;
-			selected.add(user_id[i]);
-			permutation(cnt+1, selected, user_id, banned_id, visited);
+			permutation(cnt+1, bits | (1 << i), user_id, banned_id, visited);
 			visited[i] = false;
-			selected.remove(user_id[i]);
 		}
 	}
 
